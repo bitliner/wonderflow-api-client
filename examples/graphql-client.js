950c6f40bb5c37c67376731fbe921c25304edccf
bitliner <giovanni.gaglione@gmail.com>
2017-08-10 12:05:46 +0200

    +adding login

--- examples/graphql-client.js
+++ examples/graphql-client.js
@@ -3,11 +3,15 @@ const {GraphqlClient, gql} = require('../');
 
 // CREATE THE CLIENT
 const uri = 'http://localhost:3000/graphql';
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
 const client = new GraphqlClient({
-  uri
+  uri,
+  email,
+  password,
 })
 
-// RUN A QUERY
+// EXAMPLE OF A SIMPLE QUERY
 client
   .query({
     query: gql`query {
@@ -23,3 +27,57 @@ client
   .catch((err) => {
     throw err;
   });
+
+// EXAMPLE LOGIN
+client
+  .mutate({
+    mutation: gql`
+        mutation login($email: String!, $password: String!) {
+          login(email: $email, password: $password) {
+            error
+            token
+            user {
+              _id
+              active
+              company {
+                name
+              }
+              created
+              email
+            }
+          }
+        }
+      `,
+    variables: {
+      email,
+      password,
+    },
+  })
+  .then((result) => {
+    console.log('TOKEN', result.data.login.token);
+    console.log('USER', result.data.login.user);
+  })
+  .catch((err) => {
+    throw err;
+  });
+
+
+
+  // EXAMPLE OF A MORE COMPLEX QUERY: "Access rating breakdown of a product"
+  // client
+  //   .query({
+  //     query: gql`query {
+  //         user {
+
+  //         }
+  //   }`,
+  //     variables: {
+
+  //     }
+  //   })
+  //   .then((result) => {
+  //     console.log('RESULT', result.data);
+  //   })
+  //   .catch((err) => {
+  //     throw err;
+  //   });
